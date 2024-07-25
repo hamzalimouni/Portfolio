efdacd890d266cc4a41a3480365d36d4c0d245c2
hamzalimouni <hamzalimouni9@gmail.com>
2024-07-26 01:28:33 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
