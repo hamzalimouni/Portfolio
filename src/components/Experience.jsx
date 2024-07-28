7d7f7779c535e3ec3f1ca91c40279f9404d65c6f
ELYAMOUNI Hamza <111815336+hamzalimouni@users.noreply.github.com>
2024-07-28 23:09:41 +0200

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,6 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
-
 import "react-vertical-timeline-component/style.min.css";
 
 import { styles } from "../styles";
