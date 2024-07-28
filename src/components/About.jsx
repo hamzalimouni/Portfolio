dc1ae51633ae8e7ec78fae8b5dfbc8ae25b1fa9d
ELYAMOUNI Hamza <111815336+hamzalimouni@users.noreply.github.com>
2024-07-28 23:10:01 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
