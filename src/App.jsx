fd12562f801d06eaec4a907ffd026391ebcea5b7
hamzalimouni <hamzalimouni9@gmail.com>
2025-05-21 02:38:24 +0200

    add scroll style

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import Works from './components/Works.jsx'
 
 const Container = styled.div`
   height: 100vh;
-  background-color: rebeccapurple;
+  color: white;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  &::-webkit-scrollbar{
+    display: none;
+  }
 `;
 
 function App() {
